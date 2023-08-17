95fb67d1828862e47a7d57b7e22ec46a47532cf7
RRx03 <romistargames@gmail.com>
2023-08-17 20:59:23 +0200

    Initial Commit

---
+++ TIPE_Noise/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  TIPE_Noise
+//
+//  Created by Roman Roux on 17/08/2023.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
